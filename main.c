71205bfbcaa02d90bff9e09bf9b36e1c4533e02d
Nanotech2023 <35176918+Nanotech2023@users.noreply.github.com>
2018-01-13 19:21:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "bignumber.h"
 
-char *readline()
-    {
-    int size = 16;      // Устанавливает минимальный размер строки до 16 байт
+char *readline(FILE* fp) {
+    int size = 16; // Устанавливает минимальный размер строки до 16 байт
     char *str = malloc(sizeof(char) * size);
-    if (str == NULL)
-    {
+    if (str == NULL) {
         printf("Unable to allocate memory");
         exit(EXIT_FAILURE);
     }
     int c;
     size_t len = 0;
-    while ((c = getchar()) && c != '\r' && c != '\n')
-    {
+    while ((c = fgetc(fp)) && c != '\r' && c != '\n') {
         *(str + len) = c;
         ++len;
-        if (len == size)
-        {     // Необходимо выделить больше памяти
+        if (len == size) {
+            // Необходимо выделить больше памяти
             size = size + size / 2;
-            str = (char *)realloc(str, sizeof(str)* size);
-            if (str == NULL)
-            {
-                printf("Unable to allocate memory");
+            str = (char *)realloc(str, sizeof(str) * size);
+            if (str == NULL) {
+                printf("Unable to allocate memory\n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -34,49 +29,53 @@ char *readline()
     *(str + len + 1) = '\0';
     return str;
 }
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("No file specified\n");
+        exit(EXIT_FAILURE);
+    }
 
-int main()
-{
+    FILE* fp;
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        printf("Couldn't open file \"%s\".\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+
     char* aString;
     char* bString;
     char operation;
     printf("Enter a = ");
-    aString = readline();
+    aString = readline(fp);
     printf("Enter b = ");
-    bString = readline();
+    bString = readline(fp);
     printf("\nEnter an operation\n");
-    operation = getchar();
-
+    operation = fgetc(fp);
     bigNum a = BigNum(aString);
     bigNum b = BigNum(bString);
-
     printf("a = ");
     print(&a);
     printf("b = ");
     print(&b);
-    if(operation == '+')
-    {
+    if (operation == '+') {
         bigNum aSumB = sum(&a, &b);
         printf("a + b = ");
         print(&aSumB);
         DelBigNum(&aSumB);
     }
-    if(operation == '-')
-    {
+    if (operation == '-') {
         bigNum aSubB = sub(&a, &b);
         printf("a - b = ");
         print(&aSubB);
         DelBigNum(&aSubB);
     }
-    if(operation == '*')
-    {
+    if (operation == '*') {
         bigNum aMulB = mul(&a, &b);
         printf("a * b = ");
         print(&aMulB);
         DelBigNum(&aMulB);
     }
-    if(operation == '/')
-    {
+    if (operation == '/') {
         bigNum aDivB = bigNum_div(&a, &b);
         printf("a / b = ");
         print(&aDivB);
